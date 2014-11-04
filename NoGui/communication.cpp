fda22db2209b57bd0e727b89a323bc65db8ebc78
eivinwi <eivinwi@matnat.uio.no>
2014-11-04 12:55:39 +0100

    pos-ping server

--- NoGui/communication.cpp
+++ NoGui/communication.cpp
@@ -4,6 +4,10 @@ Communication::Communication() {
 	int s = 0;
     pthread_t com_thread;
     pthread_create(&com_thread, NULL, readLoop, &s);
+
+
+    pthread_t th;
+    pthread_create(&th, NULL, writeLoop, &s);
 }
 
 Communication::~Communication() {
--- NoGui/communication.h
+++ NoGui/communication.h
@@ -38,13 +38,36 @@ class Communication {
 				socket.recv (&request);
 				std::string rpl = std::string(static_cast<char*>(request.data()), request.size());
 
-				std::cout << "Received" << rpl  << std::endl;
+				std::cout << "READ Received" << rpl  << std::endl;
 				// Do some 'work'
 				sleep(1);
 				// Send reply back to client
 				zmq::message_t reply (5);
 				memcpy ((void *) reply.data (), "ok", 2);
 				socket.send (reply);
+			}
+			return 0;
+		}	
+
+
+		static void* writeLoop(void *me) {
+			// Prepare our context and socket
+			zmq::context_t context (1);
+			zmq::socket_t socket (context, ZMQ_REQ);
+			std::cout << "WRITE Connecting to Bendiks read server..." << std::endl;
+			socket.connect ("tcp://*:5556"); //localhost:5556
+			// Do 10 requests, waiting each time for a response
+			while(true) {
+				zmq::message_t request (9);
+				memcpy ((void *) request.data (), "20 30 40", 8);
+				std::cout << "WRITE Sending pos " << request_nbr << "..." << std::endl;
+				socket.send (request);
+				// Get the reply.
+				zmq::message_t reply;
+				socket.recv (&reply);
+
+				std::string rpl = std::string(static_cast<char*>(request.data()), request.size());
+				std::cout << "WRITE Received" << rpl  << std::endl;
 			}
 			return 0;
 		}	
