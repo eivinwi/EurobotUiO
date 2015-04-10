990e829024680a89c429a2e545b5e0219fe13d90
Eivind Wikheim <eivinwi@ifi.uio.no>
2015-04-10 11:47:44 +0200

    added more test-scripts

---
+++ TestClient/fulltest.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -x  # Output executed commands
+set -e  # Make script fail as soon as one command fails
+
+# Testing main program by sending a bunch of commands via testclient.
+# If working correctly the commands should be queued up and performed chronologically.
+
+./testclient 2 0 100 100
+./testclient 2 1 250 150
+./testclient 2 2 0 0
+
+./testclient 4 3 2
+./testclient 4 4 1
+./testclient 4 5 0
+
+./testclient 4 6 3
+./testclient 4 7 4
+./testclient 4 8 3
+./testclient 4 9 4
+
+./testclient 4 10 5
+./testclient 4 11 6
+./testclient 4 12 5
+
+./testclient 4 13 7
+./testclient 4 14 8
+./testclient 4 15 7
